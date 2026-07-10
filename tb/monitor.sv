84f04a88d027fdefc0e0db35173316a0ec1a7f50
Shreya Sharma <codershreya33@gmail.com>
2026-07-10 18:42:44 +0530

    Remove FIFO overflow and underflow immediate assertions

--- tb/monitor.sv
+++ tb/monitor.sv
@@ -10,12 +10,7 @@ class monitor;
   task run();
     forever begin
       @(posedge fif.clk);
-      #1;
-      //no write when full without read
-      assert(!(fif.wr && fif.full && !fif.rd))
-       else $error("[ASSERT]:fifo overflow:write when ful");
-      assert(!(fif.rd && fif.empty))
-      else $error("[ASSERT]:fifo underflow:read when empty");
+  
       tr = new();
       tr.wr    = fif.wr;
       tr.rd    = fif.rd;
@@ -27,6 +22,8 @@ class monitor;
 
       $display("[MON]: wr=%0d rd=%0d din=%0d dout=%0d full=%0d empty=%0d",
                tr.wr, tr.rd, tr.din, tr.dout, tr.full, tr.empty);
+      $display("[MON] cnt=%0d wptr=%0d full=%0d empty=%0d wr=%0d rd=%0d",
+         fif.cnt, fif.wptr, fif.full, fif.empty, fif.wr, fif.rd);
     end
   endtask
 endclass
