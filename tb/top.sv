d0bc3e929ee9917ce8c087630c10b4f1c94bca77
Shreya Sharma <codershreya33@gmail.com>
2026-07-10 19:25:56 +0530

    Update top module
    Increased the count parameter for the environment generator and added FIFO counter and pointer signals.

--- tb/top.sv
+++ tb/top.sv
@@ -1,25 +1,30 @@
+
 module tb;
   fifo_if fif();        // Instantiate FIFO interface
   fifo dut(
   fif.clk, fif.rst, fif.wr, fif.rd,
   fif.din, fif.dout,
-  fif.full, fif.empty
+  fif.full, fif.empty,fif.cnt,fif.wptr,fif.rptr
 );
   // FIFO DUT
   initial fif.clk = 0;
   always #10 fif.clk = ~fif.clk;  // 50MHz clock
   
   environment env;
+  fifo_assertions asrt(fif);
   
   initial begin
     env = new(fif);
-    env.gen.count = 10;
+    env.gen.count = 200;
     env.run();
   end
   
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0,tb);
+    #10000;
+    $stop;
   end
 endmodule
+  
   
